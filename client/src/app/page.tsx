45c37d418ab642ea738e8193cc85dad90abe0c0c
Raj Pulapakura <raj.pulapakura@gmail.com>
2024-01-07 19:51:53 +1100

    init next app

---
+++ client/src/app/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import Image from "next/image";
+
+export default function Home() {
+  const train = () => {};
+
+  return (
+    <>
+      <button onClick={train}>Train</button>
+    </>
+  );
+}
